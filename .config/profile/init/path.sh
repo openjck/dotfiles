13811605a88676029305d1a1799566c9f0e25ffd
John Karahalis <john@johnkarahalis.com>
2023-10-26 21:57:06 -0400

    profile: Adjust $PATH precedence

--- .config/profile/init/path.sh
+++ .config/profile/init/path.sh
@@ -1,13 +1,13 @@
 # shellcheck shell=sh
 
-export PATH="$PATH:$HOME/bin"
-export PATH="$PATH:$HOME/bin/local"
-export PATH="$PATH:$HOME/bin/local/downloaded"
-export PATH="$PATH:$HOME/bin/local/temporary"
-export PATH="$PATH:$HOME/.cargo/bin"
-export PATH="$PATH:$HOME/.local/bin"
-
 if __command_exists ruby; then
-  PATH=$PATH:$(ruby -e 'puts Gem.user_dir')/bin
+  PATH=$(ruby -e 'puts Gem.user_dir')/bin:$PATH
   export PATH
 fi
+
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/bin/local/downloaded:$PATH"
+export PATH="$HOME/bin:$PATH"
+export PATH="$HOME/bin/local:$PATH"
+export PATH="$HOME/bin/local/temporary:$PATH"
