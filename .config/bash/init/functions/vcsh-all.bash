ad1cafc9fc5434e8726c9d152e7fea215808c94f
John Karahalis <john@johnkarahalis.com>
2023-12-13 22:21:23 -0500

    Bash: Explain diff. between vcsh-all and foreach

--- .config/bash/init/functions/vcsh-all.bash
+++ .config/bash/init/functions/vcsh-all.bash
@@ -1,3 +1,5 @@
+# This works like "vcsh foreach" (e.g., "vcsh foreach ls-files"), except that it
+# doesn't print a header for each repo like "vcsh foreach" does.
 function vcsh-all() {
   for REPO in $(vcsh list); do
     vcsh "$REPO" "$@"
