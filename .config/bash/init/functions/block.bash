386ed0792466f632d095c83bf3fb9711f7bf3ca1
John Karahalis <john@johnkarahalis.com>
2024-06-28 17:42:44 -0400

    Bash: Document purpose of "block" function

--- .config/bash/init/functions/block.bash
+++ .config/bash/init/functions/block.bash
@@ -1,3 +1,5 @@
+# This function simply aliases "toilet" as "block" for situations, including
+# work situations, where "toilet" would be an odd term for me to type on screen.
 function block() {
   toilet "$@"
 }
