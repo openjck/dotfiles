9b8814c7b22adb0df80341758d9cbf8f9f380a28
John Karahalis <john@johnkarahalis.com>
2024-08-04 13:24:07 -0400

    Bash: Set up Zellij completion

--- .config/bash/init/zellij.bash
+++ .config/bash/init/zellij.bash
@@ -3,3 +3,6 @@
 # See the following for more information:
 # https://zellij.dev/documentation/integration.html#bash
 eval "$(zellij setup --generate-auto-start bash)"
+
+# Set up Zellij completion.
+eval "$(zellij setup --generate-completion bash)"
