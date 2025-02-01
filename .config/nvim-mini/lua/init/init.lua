feba6a35843d692c7129d2c05eff2d14e04d0f4b
John Karahalis <john@johnkarahalis.com>
2025-02-01 12:58:30 -0500

    Neovim: Set the onedark colorscheme in nvim-mini

--- .config/nvim-mini/lua/init/init.lua
+++ .config/nvim-mini/lua/init/init.lua
@@ -1,2 +1,8 @@
 require('init.mini')
+
+-- Enable and configure plugins. This must be done before some things, like the
+-- colorscheme, are set.
 require('init.plugins')
+
+-- Load general configurations.
+require('init.appearance')
--- .config/nvim-mini/lua/init/plugins/init.lua
+++ .config/nvim-mini/lua/init/plugins/init.lua
@@ -1,1 +1,5 @@
+-- mini plugins must be installed first because mini.deps is among them and
+-- mini.deps is used to install all other plugins.
 require('init.plugins.mini')
+
+require('init.plugins.colors')
