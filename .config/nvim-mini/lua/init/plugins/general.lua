9b438cd25d27e32a5babfab7c9da0035de2ddf44
John Karahalis <john@johnkarahalis.com>
2025-02-08 22:06:55 -0500

    Neovim: Document plugin choices in nvim-mini

--- .config/nvim-mini/lua/init/plugins/general.lua
+++ .config/nvim-mini/lua/init/plugins/general.lua
@@ -1,3 +1,10 @@
+-- mini.nvim does offer a fuzzy finder plugin,[1] but I don't believe it's
+-- possible to use it to create a fuzzy finder with a custom source. With
+-- telescope, I create a custom fuzzy finder fot dotfiles.
+--
+-- Plus, I've always wanted to use telescope.
+--
+-- [1] https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-fuzzy.md
 MiniDeps.add({
   source = 'nvim-telescope/telescope.nvim',
   checkout = '0.1.x',
@@ -12,6 +19,17 @@ MiniDeps.add({
   },
 })
 
+-- I considered using newer projects like nvim-lint[1] and conform,[2] but as
+-- of the time of this writing (2025-02-08), nvim-lint currently cannot easily
+-- use a copy of ESLint that exists in node_modules. That's important, because
+-- different projects often use differnt versions of ESLint.
+--
+-- ale is also a more mature project, and I've enjoyed working with it. It does
+-- ship with its own Language Server client, which is not needed in Neovim
+-- since Neovim ships its own, but ale's can be disabled.
+--
+-- [1] https://github.com/mfussenegger/nvim-lint
+-- [2] https://github.com/stevearc/conform.nvim
 MiniDeps.add({
   source = 'dense-analysis/ale'
 })
