56d3b0e28020332c2f21f5de9ad572e49248d2ca
John Karahalis <john@johnkarahalis.com>
2025-02-09 19:08:47 -0500

    Neovim: Migrate local init support to new profile

--- .config/nvim-previous-to-be-migrated/init.vim
+++
@@ -1,4 +0,0 @@
-" Source private, non-pushed, local configurations, if any.
-if filereadable(expand('<sfile>:h') . '/init.local.vim')
-  source <sfile>:h/init.local.vim
-endif
--- .config/nvim/lua/init/init.lua
+++ .config/nvim/lua/init/init.lua
@@ -9,3 +9,9 @@ require('init.appearance')
 require('init.editing')
 require('init.leader')
 require('init.spacing')
+
+-- Source private, non-pushed, local configurations, if any.
+--
+-- To be found and used, the local configurations must exist in a file named
+-- $XDG_CONFIG_HOME/nvim/lua/init/local/init.lua.
+pcall(require, 'init.local')
