bb2325cbbcf547a1be5b9f5df6ed5ec39b0aa39a
John Karahalis <john@johnkarahalis.com>
2025-02-11 21:48:22 -0500

    Neovim: Add note about fuzzy/pick as telescope alt

--- .config/nvim/lua/init/plugins/general.lua
+++ .config/nvim/lua/init/plugins/general.lua
@@ -1,12 +1,12 @@
 local add = MiniDeps.add
 
--- mini.nvim does offer a fuzzy finder plugin,[1] but I don't believe it's
--- possible to use it to create a fuzzy finder with a custom source. With
--- telescope, I create a custom fuzzy finder fot dotfiles.
---
--- Plus, I've always wanted to use telescope.
---
--- [1] https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-fuzzy.md
+-- The mini.nvim project offers a fuzzy finder (mini.fuzzy) and picker
+-- (mini.pick) which together appear to be capable of solving the same problems
+-- telescope does. It may even be possible to create a custom picker for my
+-- dotfiles, as I do with telescope. However, I prefer the way telescope
+-- appears, especially its multi-pane interface, I love all the much more
+-- comprehensive set of pickers it provides (run :Telescope to see them), and
+-- it's just more fun! I had been wanting to use telescope for a while.
 add({
   source = 'nvim-telescope/telescope.nvim',
   checkout = '0.1.x',
