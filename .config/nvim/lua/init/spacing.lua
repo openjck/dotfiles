624fd47b5c2c90d1628185b9767c405de7b2d593
John Karahalis <john@johnkarahalis.com>
2025-02-16 20:24:34 -0500

    Neovim: Use vim.opt, not vim.o in spacing.lua

--- .config/nvim/lua/init/spacing.lua
+++ .config/nvim/lua/init/spacing.lua
@@ -7,7 +7,7 @@
 -- the backspace. If changing a value here does not seem to have the desired
 -- effect, that may be why. For example, it may appear that the value of
 -- softtabstop is being ignored because smarttab and autoindent are enabled.
-vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
