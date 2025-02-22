1ca88c212fcc99fd235fbb969923691a1e886282
John Karahalis <john@johnkarahalis.com>
2025-02-21 20:11:10 -0500

    Neovim: Don't use keywords in hipatterns itself

--- .config/nvim/lua/init/plugins/mini/hipatterns.lua
+++ .config/nvim/lua/init/plugins/mini/hipatterns.lua
@@ -1,5 +1,9 @@
 local hipatterns = require('mini.hipatterns')
 
+function unbusted(busted_string)
+  return busted_string:gsub('<busted>', '')
+end
+
 -- This configuration is based on an example configuration published in the
 -- mini.hipatterns README.
 --
@@ -7,15 +11,38 @@ local hipatterns = require('mini.hipatterns')
 hipatterns.setup({
   highlighters = {
 
-    -- Highlight various terms when they appear separately from other text. Lua
-    -- patterns are used. Lua patterns are intended to be simpler, more
-    -- lightweight alternatives to regular expressions.
+    -- Highlight terms that appear separately from other text.
     --
-    -- INPROG is a keyword I often add in comments to remind myself of things
-    -- that should be addressed before I open a pull request or merge request.
-    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-    todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
-    inprog = { pattern = '%f[%w]()INPROG()%f[%W]', group = 'MiniHipatternsHack' },
+    -- Lua patterns are used. Lua patterns are intended to be like simpler
+    -- regular expressions that are easier to parse. I personally find the
+    -- syntax confusing, but that's probably only because I have experience with
+    -- regular expressions. If I had never used regular expressions, I'm sure
+    -- Lua patterns would seem straightforward. In any case, I don't really
+    -- understand the syntax, but it's taken from examples in the hipatterns
+    -- documentation.
+    --
+    -- Below, each value of a "pattern" property contains the string "<busted>",
+    -- which is removed by the "unbusted" function. That's only done so that
+    -- searches for those terms don't match this file itself. Underscores are
+    -- used in property names for the same reason.
+    --
+    -- IN<busted>PROG (yes, mentally remove the string "<busted>" while reading
+    -- this) is a keyword I often use to mark work in progress that should be
+    -- modified before a pull request / merge request is opened.
+    fix_me = {
+      pattern = unbusted('%f[%w]()FIX<busted>ME()%f[%W]'),
+      group = 'MiniHipatternsFixme',
+    },
+
+    to_do = {
+      pattern = unbusted('%f[%w]()TO<busted>DO()%f[%W]'),
+      group = 'MiniHipatternsTodo',
+    },
+
+    in_prog = {
+      pattern = unbusted('%f[%w]()IN<busted>PROG()%f[%W]'),
+      group = 'MiniHipatternsHack',
+    },
 
     -- Highlight hex color strings (`#rrggbb`) using that color.
     hex_color = hipatterns.gen_highlighter.hex_color(),
