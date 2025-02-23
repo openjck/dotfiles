a4a355d5e3494d02d864a531ba49de9324c911bd
John Karahalis <john@johnkarahalis.com>
2025-02-22 22:30:58 -0500

    Bash: Open neogit in full screen in neovim-git

--- .config/bash/init/functions/neovim-git.bash
+++ .config/bash/init/functions/neovim-git.bash
@@ -1,3 +1,5 @@
 function neovim-git() {
-  nvim -c Neogit
+  # Open neogit in Neovim, with neogit replacing the normal startup screen
+  # (i.e., being full screen) rather than opening beneath it.
+  nvim -c 'lua require("neogit").open({ kind = "replace" })'
 }
