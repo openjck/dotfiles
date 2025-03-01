fcf48ec25eaec7f5fc04cc069c6846711666b32a
John Karahalis <john@johnkarahalis.com>
2025-02-28 21:00:07 -0500

    Neovim: In onedark, make floats stand out more

--- .config/nvim/lua/init/plugins/colorschemes/onedark.lua
+++ .config/nvim/lua/init/plugins/colorschemes/onedark.lua
@@ -1,1 +1,8 @@
 MiniDeps.add('navarasu/onedark.nvim')
+
+require('onedark').setup  {
+    highlights = {
+      -- Make diagnostic floats and other floats stand out more.
+      NormalFloat = { bg = '$bg3' },
+    },
+}
