5604ae7740bbec31077105803ec65349fdf1b7fd
John Karahalis <john@johnkarahalis.com>
2025-03-09 13:16:06 -0400

    Neovim: Migrate replace with register ability

--- .config/nvim-previous-to-be-migrated/init/plugins/general.vim
+++
@@ -1,4 +0,0 @@
-" mini.nvim apparently has some similar functionality in mini.operators. I don't
-" know how it stacks up against this plugin, although I don't use this plugin
-" often, anyway.
-Plug 'vim-scripts/ReplaceWithRegister'
---
+++ .config/nvim/lua/init/plugins/mini/operators.lua
@@ -0,0 +1,7 @@
+-- This plugin offers a variety of features, but the one I'm most interested in
+-- is the feature for more easily replacing text with the contents of a
+-- register. I previously used the "vim-scripts/ReplaceWithRegister" plugin for
+-- that.
+local operators = require('mini.operators')
+
+operators.setup()
