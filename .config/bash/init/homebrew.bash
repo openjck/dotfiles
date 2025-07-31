5a8917f3dd216f579cf967c21a1f90dd48d04b1c
John Karahalis <john@johnkarahalis.com>
2025-07-31 17:01:53 -0400

    Bash: Fix Homebrew error on systems without it

--- .config/bash/init/homebrew.bash
+++ .config/bash/init/homebrew.bash
@@ -1,2 +1,4 @@
-# Homebrew's setup script recommends including this line in .bashrc.
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if [[ -f /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+  # Homebrew's setup script recommends including this line in .bashrc.
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
