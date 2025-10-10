1e90056e8e944cab011913f12636b791fead2f29
John Karahalis <john@johnkarahalis.com>
2025-10-09 20:58:57 -0400

    Neovim: Change default spacing to 4

--- .config/nvim/lua/openjck/init/spacing.lua
+++ .config/nvim/lua/openjck/init/spacing.lua
@@ -8,6 +8,6 @@
 -- effect, that may be why. For example, it may appear that the value of
 -- softtabstop is being ignored because smarttab and autoindent are enabled.
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
