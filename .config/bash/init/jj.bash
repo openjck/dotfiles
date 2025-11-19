b6f537345a6b1dbdc362fdd15f7e5b1df6c28877
John Karahalis <john@johnkarahalis.com>
2025-11-18 19:15:13 -0500

    Bash: Add jj config

---
+++ .config/bash/init/jj.bash
@@ -0,0 +1,6 @@
+# Enable standard completion, which is more stable than dynamic completion but
+# less featureful.
+#
+# More information about the tradeoffs:
+# https://jj-vcs.github.io/jj/latest/install-and-setup/#command-line-completion
+source <(jj util completion bash)
