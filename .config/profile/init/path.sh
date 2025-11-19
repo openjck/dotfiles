0ffe4a1f2146fbbaec2f6de84d6d4d87c0058c98
John Karahalis <john@johnkarahalis.com>
2025-11-18 19:13:30 -0500

    bin, shell: Add flatpak alias bins to $PATH

--- .config/profile/init/path.sh
+++ .config/profile/init/path.sh
@@ -5,10 +5,16 @@ if __command_exists ruby; then
   export PATH
 fi
 
+# The order of these lines matters.
+#
+# We are prepending to the $PATH, so each line takes priority over lines that
+# came before it. If a bin is found in multiple directories along the $PATH, the
+# bin from the directory lowest on this list will be used.
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/bin/local/downloaded:$PATH"
 export PATH="$HOME/apps/appimage:$PATH"
+export PATH="$HOME/bin/flatpak:$PATH"
+export PATH="$HOME/bin/local/downloaded:$PATH"
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/bin/local:$PATH"
 export PATH="$HOME/bin/local/temporary:$PATH"
