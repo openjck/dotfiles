a67bd1fb29d39a99cb7ed9939e433e280d4af7d5
John Karahalis <john@johnkarahalis.com>
2026-01-15 00:53:00 -0500

    Bash: Ignore git directories when using rg

--- .config/bash/init/functions/rg.bash
+++ .config/bash/init/functions/rg.bash
@@ -1,5 +1,7 @@
 # Run rg with custom options and page the results. If there are no results,
 # return immediately.
+#
+# --glob=!.git/ tells rg to ignore files within ".git/" directories themselves.
 function rg() {
-  command rg --hidden --pretty "$@" | ifne less
+  command rg --hidden --pretty --glob=!.git/ "$@" | ifne less
 }
