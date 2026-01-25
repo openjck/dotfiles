76680ef7250ec566534245ca48c97bee0b2cb36a
John Karahalis <john@johnkarahalis.com>
2026-01-25 16:07:42 -0500

    Bash: Enable checkwinsize manually to be safe

--- .config/bash/init/options.bash
+++ .config/bash/init/options.bash
@@ -4,6 +4,24 @@ set -o vi
 # Free up CTRL-S so that it can be used for forward searches.
 stty -ixon
 
+# According to the man page of the version of Bash that I use, when this option
+# is set:
+#
+#   bash checks the window size after each external (non-builtin) command and,
+#   if necessary, updates the values of LINES and COLUMNS.
+#
+# It also says:
+#   This option is enabled by default
+#
+# On Debian-based systems, /etc/skel/.bashrc enables this option even though the
+# Bash man page says it's enabled by default. That could be because the file is
+# outdated, or it could be because this option is disabled on Debian-based
+# systems for some other reason. Regardless, I feel it's safest to enable it
+# here, especially because I may use a system that does not have it enabled for
+# some reason, and especially because I do sometimes have issues where text
+# reflows strangely in the terminal after it's resized.
+shopt -s checkwinsize
+
 # Enable globstar (**) support.
 shopt -s globstar
 
