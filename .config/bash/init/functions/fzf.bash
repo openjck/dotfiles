a737537921204424d70836cdac2cee2f5e6dea8c
John Karahalis <john@johnkarahalis.com>
2026-01-25 13:15:41 -0500

    Bash: Enable fzf history

--- .config/bash/init/functions/fzf.bash
+++ .config/bash/init/functions/fzf.bash
@@ -1,6 +1,9 @@
 # fzf-tmux is a command that ships with fzf. It opens fzf in a tmux pane,
 # allowing shell history to remain on screen while fzf is used. When tmux is not
 # being used, it transparently works like normal the normal fzf command.
+#
+# With the history option set, CTRL-P can be used to quickly access past search
+# terms, and CTRL-N can be used to move forward in history.
 function fzf() {
-  fzf-tmux "$@"
+  fzf-tmux -- --history="$XDG_DATA_HOME/fzf/history" "$@"
 }
--- .config/bash/init/fzf.bash
+++ .config/bash/init/fzf.bash
@@ -3,5 +3,9 @@
 # https://github.com/junegunn/fzf/issues/337#issuecomment-136383876
 export FZF_DEFAULT_COMMAND='find .'
 
+# This environment variable is not used by fzf itself, but rather by the "fzf"
+# function with the --history option.
+export FZF_HISTORY_FILE=$XDG_DATA_HOME/history
+
 # Enable Bash integration.
 eval "$(fzf --bash)"
