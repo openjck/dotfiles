a541d0ce54a8464fb9a714e220c9fb5298c9c344
John Karahalis <john@johnkarahalis.com>
2026-02-13 16:58:54 -0500

    bin: Reorganize scripts
    This commit moves scripts into one of the following two directories
    based on whether others are likely to find the script useful:
    
        $HOME/bin/general:  Scripts others might find useful. Others may be
                            interested in downloading and running some of
                            these scripts.
        $HOME/bin/personal: Scripts that others are unlikely to ever use
                            because, among other things, they may be highly
                            particular to my setup. They're not private or
                            secretive, and others can learn from the code if
                            they wish, but others are unlikely to want to
                            run them as-is.
    
    Additionally, vcsh-related scripts have been moved back to
    $XDG_CONFIG_HOME/vcsh because they don't quite fit either of the above
    categories.

--- .config/profile/init/path.sh
+++ .config/profile/init/path.sh
@@ -1,22 +1,22 @@
 # shellcheck shell=sh
 
+# Line order matters here.
+#
+# We are **prepending** to $PATH, so each line takes priority over lines that
+# come before it. The lowest line has the greatest precedence. If an executable
+# is found in multiple of these directories, the executable from the directory
+# lowest on this list will be used.
 if __command_exists ruby; then
   PATH=$(ruby -e 'puts Gem.user_dir')/bin:$PATH
-  export PATH
 fi
+PATH="$HOME/.local/bin:$PATH"
+PATH="$HOME/bin/general:$PATH"
+PATH="$HOME/apps/appimage:$PATH"
+PATH="$HOME/bin/personal/flatpak:$PATH"
+PATH="$HOME/bin/personal/local/downloaded:$PATH"
+PATH="$HOME/bin/personal:$PATH"
+PATH="$HOME/bin/personal/vcsh:$PATH"
+PATH="$HOME/bin/personal/local:$PATH"
+PATH="$HOME/bin/personal/local/temporary:$PATH"
 
-# The order of these lines matters.
-#
-# We are prepending to the $PATH, so each line takes priority over lines that
-# came before it. If a bin is found in multiple directories along the $PATH, the
-# bin from the directory lowest on this list will be used.
-export PATH="$HOME/.cargo/bin:$PATH"
-export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/apps/appimage:$PATH"
-export PATH="$HOME/bin/flatpak:$PATH"
-export PATH="$HOME/bin/local/downloaded:$PATH"
-export PATH="$HOME/bin:$PATH"
-export PATH="$HOME/bin/vcsh:$PATH"
-export PATH="$HOME/bin/vcsh/dotfiles-openjck-setup:$PATH"
-export PATH="$HOME/bin/local:$PATH"
-export PATH="$HOME/bin/local/temporary:$PATH"
+export PATH
