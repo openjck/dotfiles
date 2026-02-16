a5fa3fb62daf11422f367a172670568258cbd8ab
John Karahalis <john@johnkarahalis.com>
2026-02-15 21:42:36 -0500

    Bash: Update directories env vars for new bin org

--- .config/bash/init/directories.bash
+++ .config/bash/init/directories.bash
@@ -1,19 +1,29 @@
 # Add environment variables for LGTD directories.
 export D_INBOX=$HOME/LGTD/inboxes/main
-export D_PROJECT_SUPPORT=$HOME/LGTD/project-support/main
 
-# Add environment variables for other general-purpose directories.
-export D_BASH_FUNCTIONS_GLOBAL=$XDG_CONFIG_HOME/bash/init/functions
-export D_BASH_FUNCTIONS_LOCAL=$XDG_CONFIG_HOME/bash/init/functions/local
+# Add environment variables for Bash functions.
+export D_BASH_FUNCTIONS=$XDG_CONFIG_HOME/bash/init/functions
+export D_BASH_FUNCTIONS_LOCAL=$D_BASH_FUNCTIONS/local
+
+# Add environment variables for ~/bin directories.
 export D_BIN=$HOME/bin
-export D_BIN_LOCAL=$HOME/bin/local
-export D_BIN_DOWNLOADED=$HOME/bin/local/downloaded
-export D_BIN_TEMPORARY=$HOME/bin/local/temporary
-export D_BIN_VCSH=$HOME/bin/vcsh
-export D_BIN_DOTFILES_OPENJCK_SETUP=$HOME/bin/vcsh/dotfiles-openjck-setup
+export D_BIN_GENERAL=$D_BIN/general
+export D_BIN_PERSONAL=$D_BIN/personal
+export D_BIN_PERSONAL_FLATPAK=$D_BIN_PERSONAL/flatpak
+export D_BIN_PERSONAL_LOCAL=$D_BIN_PERSONAL/local
+export D_BIN_PERSONAL_LOCAL_DOWNLOADED=$D_BIN_PERSONAL_LOCAL/downloaded
+export D_BIN_PERSONAL_LOCAL_TEMPORARY=$D_BIN_PERSONAL_LOCAL/temporary
+
+# Add environment variables for config directories.
+export D_CONFIG_NVIM=$XDG_CONFIG_HOME/nvim
+export D_CONFIG_VCSH=$XDG_CONFIG_HOME/vcsh
+export D_CONFIG_VCSH_HOOKS_AVAILABLE=$D_CONFIG_VCSH/hooks-available
+export D_CONFIG_VCSH_HOOKS_ENABLED=$D_CONFIG_VCSH/hooks-enabled
+
+# Add environment variables for other directories.
 export D_DEVEL=$HOME/devel
-export D_REPOS=$HOME/devel/repos
-export D_NVIM=$XDG_CONFIG_HOME/nvim
+export D_DEVEL_REPOS=$HOME/devel/repos
+export D_DOTFILES_OPENJCK_SETUP=$D_CONFIG_VCSH_HOOKS_AVAILABLE/dotfiles-openjck-setup
 
 # Add environment variables for Windows directories if WSL is being used.
 if __in_wsl; then
