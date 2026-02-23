b9c58836a867ca26f70d0a0c32bf151bd535e201
John Karahalis <john@johnkarahalis.com>
2026-02-23 17:05:24 -0500

    Neovim: Use mini.basics window mappings

--- .config/nvim/lua/openjck/init/plugins/mini/basics.lua
+++ .config/nvim/lua/openjck/init/plugins/mini/basics.lua
@@ -1,3 +1,10 @@
 local basics = require('mini.basics')
 
-basics.setup()
+basics.setup({
+  mappings = {
+    -- Use <C-hjkl> to navigate between panes (just like the keybindings
+    -- vim-tmux-navigator offers, making vim-tmux-navigator unnecessary), and
+    -- use <C-arrow> to adjust pane size.
+    windows = true,
+  }
+})
