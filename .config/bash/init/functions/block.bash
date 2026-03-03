00a836baa83f1fae258ed08f018f7885e6e72279
John Karahalis <john@johnkarahalis.com>
2026-03-03 12:46:57 -0500

    Bash, bin/personal: Make "block" a shell script

--- .config/bash/init/functions/block.bash
+++
@@ -1,5 +0,0 @@
-# This function simply aliases "toilet" as "block" for situations, including
-# work situations, where "toilet" would be an odd term for me to type on screen.
-function block() {
-  toilet "$@"
-}
