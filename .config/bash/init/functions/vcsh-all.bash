38e313a46e46808f09d276f588c411e2b19ca5f1
John Karahalis <john@johnkarahalis.com>
2026-03-15 21:43:23 -0400

    bin, Bash: Rewrite vcsh-all as a shell script

--- .config/bash/init/functions/vcsh-all.bash
+++
@@ -1,9 +0,0 @@
-# TODO: This should probably be rewritten and published as a shell script.
-
-# This works like "vcsh foreach" (e.g., "vcsh foreach ls-files"), except that it
-# doesn't print a header for each repo like "vcsh foreach" does.
-function vcsh-all() {
-  for REPO in $(vcsh list); do
-    vcsh "$REPO" "$@"
-  done
-}
