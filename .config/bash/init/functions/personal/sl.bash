f2659c4ae54c61450484f419724e2984ceff85e2
John Karahalis <john@johnkarahalis.com>
2026-03-26 13:40:28 -0400

    Bash, bin: Convert sl w/ random options to script

--- .config/bash/init/functions/personal/sl.bash
+++ .config/bash/init/functions/personal/sl.bash
@@ -1,22 +1,7 @@
-# Run sl with a random combination of arguments and allow it to be interrupted
-# with Ctrl+C.
+# Use slr (Steam Locomotive Random) and allow exiting with CTRL+C.
+#
+# Intentionally drop all other arguments, since "slr" doesn't accept any other
+# arguments that might have been intended for ls.
 function sl() {
-  # Allow interrupting sl with Ctrl+C.
-  local ARGUMENTS=('-e')
-
-  # All options that sl accepts, except -e, which is already set above.
-  local SL_OPTIONS=(
-    '-a'
-    '-l'
-    '-F'
-  )
-
-  for SL_OPTION in "${SL_OPTIONS[@]}"; do
-    # Flip a coin...
-    if [[ $((RANDOM % 2)) -eq 0 ]]; then
-      ARGUMENTS+=("$SL_OPTION")
-    fi
-  done
-
-  command sl "${ARGUMENTS[@]}"
+  slr -e
 }
