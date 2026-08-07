94b548011222e2c4bc97666a780d9df4474e85e7
John Karahalis <john@johnkarahalis.com>
2026-08-06 20:17:16 -0400

    Bash: Use --all, --long by default when using eza

--- .config/bash/init/functions/personal/eza.bash
+++ .config/bash/init/functions/personal/eza.bash
@@ -1,9 +1,11 @@
 function eza() {
   command eza \
+    --all \
     --git \
     --group \
     --group-directories-first \
     --header \
+    --long \
     --octal-permissions \
     --time-style=long-iso \
     "$@"
